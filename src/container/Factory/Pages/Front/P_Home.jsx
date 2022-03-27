3844ee11b4d6cdeeca84c22149842f7cc2c707ae
akash <ak.chourasia.007@gmail.com>
2022-03-27 14:01:35 +0530

    init commit

---
+++ src/container/Factory/Pages/Front/P_Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import M_HeaderBar from '../../Molecules/M_HeaderBar'
+export default function P_Home() {
+    return (
+        <div>
+            <M_HeaderBar/>
+        </div>
+    )
+}
